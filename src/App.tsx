4670070fa9c2db386e52abb8ceed61679a719dd7
Renan Felliphe de Moura Diogo Silva <felliphemouraa@gmail.com>
2026-02-09 20:53:39 -0300

    teste

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+export function App(){
+    return (
+        <>
+            <div>
+                <h1> Cadastro de Cliente</h1>
+                <form>
+                    <div>
+                        <label htmlFor="nome">Nome</label>
+                        <input type="text" name="nome" id="nome"/>
+                    </div>
+                    <div>
+                        <label htmlFor="email">Email</label>
+                        <input type="text" name="email" id="email"/>
+                    </div>
+                    <div>
+                        <label htmlFor="cep">Cep</label>
+                        <input type="text" name="cep" id="cep"/>
+                    </div>
+                    <div>
+                        <label htmlFor="rua">Rua</label>
+                        <input type="text" name="rua" id="rua"/>
+                    </div>
+                    <div>
+                        <label htmlFor="numero">Número</label>
+                        <input type="text" name="numero" id="numero"/>
+                    </div>
+                    <div>
+                        <label htmlFor="cidade">Cidade</label>
+                        <input type="text" name="cidade" id="cidade"/>
+                    </div>
+                    <div>
+                        <label htmlFor="bairro">Bairro</label>
+                        <input type="text" name="bairro" id="bairro"/>
+                    </div>
+                    <div>
+                        <label htmlFor="uf">UF</label>
+                        <input type="text" name="uf" id="uf"/>
+                    </div>
+                    <div>
+                        <button type="reset">Limpar</button>
+                        <button type="submit">Enviar</button>
+                    </div>
+                </form>
+            </div>
+        </>
+    )
 }
-
-export default App
